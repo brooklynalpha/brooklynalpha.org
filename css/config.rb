c84c6ee89bd6d0e9f31e4714bde736e39a98467b
Tom Sartain <tomsartain@gmail.com>
2011-09-24 17:02:52 -0400

    Playing with Compass' configs for URL helpers. Neat!

--- css/config.rb
+++ css/config.rb
@@ -2,13 +2,24 @@
 #
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/2011"
+
 css_dir = "."
 sass_dir = "src"
+
+# Images
 images_dir = "../images"
+
+# Fonts
+fonts_dir = "../fonts"
+
+# Javascript
 javascripts_dir = "../js"
+
+# Output
 output_style = :expanded
 line_comments = false
+
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 preferred_syntax = :scss
