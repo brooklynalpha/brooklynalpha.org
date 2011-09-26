53dbb3b4ee38cfded8209c75125da4d4b584def9
Tyler Gaw <me@tylergaw.com>
2011-09-25 20:37:22 -0400

    Adding registration closed message to home

--- js/app.js
+++ js/app.js
@@ -10,6 +10,25 @@ var APP = (function () {
 		$('[role=banner] a h2').fitText(1.3, {
 			'minFontSize': '19'
 		});
+		
+		$('#conferenceinfo a').click(function (e) {
+			var elem = $('<p />', {
+				'class': 'registration-closed',
+				text: 'We\'ll open up registration tomorrow, Sept. 26th at 1:00pm EST'
+			})
+			
+			if ($('p.registration-closed').length === 0) {
+				$(this).before(elem);
+				
+				setTimeout(function () {
+					$('p.registration-closed').fadeOut(100, function () {
+						$(this).remove();
+					});
+				}, 5000);
+			}
+			
+			e.preventDefault();
+		});
 	};
 	
 	return ret;
